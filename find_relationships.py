cef0a5de7c51cb38f5b83705685fe42a2a5fe907
Ernir <e.ernir@gmail.com>
2016-05-21 17:40:15 +0000

    Store data by user IDs rather than screen names

--- find_relationships.py
+++ find_relationships.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 import os
 import json
 from time import time
@@ -28,38 +30,40 @@ def discover_icelanders(api, known_users):
     iceland_place_id = "c3932d3da7922986"  # Predefined by Twitter
     tweets = api.search(q="place:{0}".format(iceland_place_id), count=100)
     for tweet in tweets:
-        if looks_icelandic(tweet.user.location) and tweet.user.screen_name not in known_users:
-            known_users.append(tweet.user.screen_name)
+        if looks_icelandic(tweet.user.location) and tweet.user.id not in known_users:
+            known_users.append(tweet.user.id)
     return known_users
 
 
-def create_new_users(api, relationships, screen_names, verbose=False):
+def create_new_users(api, relationships, user_ids, verbose=False):
     """
-    Adds the usernames
+    Adds the given user ids to the relationships dictionary if they are Icelanders.
     """
-    for name in screen_names:
-        if name not in relationships:
-            user = api.get_user(screen_name=name)
+    for user_id in user_ids:
+        if user_id not in relationships:
+            user = api.get_user(user_id=user_id)
             if looks_icelandic(user.location):
                 if verbose:
-                    print("Creating user {0}".format(name))
-                    relationships[name] = []
+                    print("Creating user {0}".format(user_id))
+                relationships[str(user_id)] = []
     return relationships
 
 
-def discover_followers(api, relationships, screen_name):
+def discover_followers(api, relationships, user_id, verbose=False):
     """
-    Uses the given Tweepy API object instance to find the followers of the user with the given screen_name.
+    Uses the given Tweepy API object instance to find the followers of the user with the given user_id.
     Updates and returns the given "relationships" dict.
     """
+    new_relationships = []
     try:
-        for user in tweepy.Cursor(api.followers, screen_name=screen_name).items():
-            if looks_icelandic(user.location):
-                relationships = create_new_users(api, relationships, [user.screen_name])
-                relationships[screen_name].append(user.screen_name)
+        for many_users in tweepy.Cursor(api.followers_ids, user_id=user_id).pages():
+            relationships = create_new_users(api, relationships, many_users, verbose=verbose)
+            print(many_users)
+            new_relationships.extend(many_users)
     except tweepy.error.TweepError as error:
         print(str(error) + ", ignored")
         pass  # The API tends to emit the response "buffering", which Tweepy does not understand. Blissfully ignoring!
+    relationships[str(user_id)] = new_relationships
     return relationships
 
 
@@ -72,9 +76,9 @@ def looks_icelandic(location_string):
 
 def main():
     # Some hard-coded variables that probably should be parameters
-    json_filename = "relationships.json"
+    json_filename = "relationships_by_id.json"
     verbose = True
-    time_limit = 60*60*2
+    time_limit = 0  #60*60*7
 
     start_time = time()
 
@@ -88,24 +92,24 @@ def main():
     api = get_tweepy_instance()
     if verbose:
         print("Searching for tweets in Iceland")
-    icelandic_names = discover_icelanders(api, list(relationships.keys()))
+    icelanders = discover_icelanders(api, list(relationships.keys()))
     if verbose:
         print("Creating new users")
-    relationships = create_new_users(api, relationships, icelandic_names, True)
+    relationships = create_new_users(api, relationships, icelanders, True)
 
     # Find users whose follower data we still haven't stored, and remedy the situation
-    without_followers = [name for name in relationships.keys() if len(relationships[name]) == 0]
-    for name in without_followers:
+    without_followers = [user_id for user_id in relationships.keys() if len(relationships[user_id]) == 0]
+    for user_id in without_followers:
         if verbose:
-            print("Looking for followers for {0}".format(name))
-        relationships = discover_followers(api, relationships, name)
-        # Store follower data on the fly
-        with open(json_filename, "w") as relationship_file:
-            json.dump(relationships, relationship_file, sort_keys=True, indent=4)
+            print("Looking for followers for {0}".format(user_id))
+        relationships = discover_followers(api, relationships, user_id, verbose)
 
         print("Time elapsed: {0}".format(time()-start_time))
         if time() - start_time > time_limit:
             break
+    # Store the new data
+    with open(json_filename, "w") as relationship_file:
+        json.dump(relationships, relationship_file, indent=4)
 
 
 if __name__ == '__main__':
